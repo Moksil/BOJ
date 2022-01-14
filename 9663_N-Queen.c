05ef6177a3e7eea0fcf0390750592921fc001f1d
Sungmin <sungmin@Sungmins-MacBook-Air.local>
2022-01-15 02:12:35 +0900

    9663

---
+++ 9663_N-Queen.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int check(int *lst, int row)
+{
+	for (int i = 0; i < row; i++)
+		if ((lst[i] == lst[row]) || (abs(lst[row] - lst[i]) == row - i))
+			return (0);
+	return (1);
+}
+
+int tracking(int *lst, int N, int row)
+{
+	int ret = 0;
+
+	if (row == N)
+		return (1);
+	for (int i = 0; i < N; i++)
+	{
+		lst[row] = i;
+		if (check(lst, row))
+			ret += tracking(lst, N, row + 1);
+	}
+	return (ret);
+}
+
+int main(void)
+{
+	int N;
+	int *lst;
+
+	scanf("%d", &N);
+	lst = (int *)malloc(sizeof(int) * N);
+	printf("%d", tracking(lst, N, 0));
+	return (0);
+}
---
+++ 9663_N-Queen.py
@@ -0,0 +1,20 @@
+def tracking(lst, N, row):
+	ret = 0
+	if row == N:
+		return 1
+	remain = [i for i in range(N)]
+	for i in range(row):
+		remain.remove(lst[i])
+	for i in remain:
+		for j in range(row):
+			if lst[j] == i or abs(i - lst[j]) == row - j:
+				break
+		else:
+			lst[row] = i
+			ret += tracking(lst, N, row + 1)
+	return ret
+
+N = int(input())
+lst = [0] * N
+
+print(tracking(lst, N, 0))
